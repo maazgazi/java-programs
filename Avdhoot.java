1fb16acc4eaf30e1a0f6daa937a4b153b0ccb603
maaz gazi <maazgazi38@gmail.com>
2026-07-04 12:02:37 +0530

    logic

---
+++ Avdhoot.java
@@ -0,0 +1,31 @@
+
+
+class Avdhoot
+{
+
+    public static void main(String[] args) {
+        
+        int x=10;
+
+        System.out.println(x);
+
+        Integer k = x   ;//  auto-boxing
+
+
+
+        System.out.println(k);
+
+        int num3 = k ; //unbboxing
+
+        System.out.println(num3);
+
+
+        String num = "12";
+
+        int num2 = Integer.parseInt(num);
+
+        System.out.println(num2*2);
+
+
+    }
+}
